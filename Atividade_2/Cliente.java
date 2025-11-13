3e51ad2231e09a81a5012419334e2f3d9aa559ff
Hevellyn <hevellynkarinneribeirocastro@gmail.com>
2025-11-12 23:18:38 -0300

    Add files via upload

---
+++ Atividade_2/Cliente.java
@@ -0,0 +1,26 @@
+public class Cliente {
+    private String nome;
+    private String email;
+
+    void clienteStatus(){
+        System.out.println("Nome do cliente: " +getNome());
+        System.out.println("Email do cliente: " + getEmail());
+    }
+   public Cliente(String nome, String email){
+       this.nome = nome;
+       this.email = email;
+   }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
---
+++ Atividade_2/Produto.java
@@ -0,0 +1,43 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int estoque;
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public double getPreco(){
+        return this.preco;
+    }
+
+    public void setPreco(double preco){
+         this.preco = preco;
+    }
+    public int getEstoque(){
+        return this.estoque;
+    }
+    public void setEstoque(int estoque){
+        this.estoque = estoque;
+    }
+    public void adicionarEstoque(int quantidade){
+        if(quantidade>0){
+            estoque = estoque + quantidade;
+        }
+    }
+
+
+    void produtoStatus(){
+        System.out.println("Nome do produto: " +getNome());
+        System.out.println("Preco do produto: " + getPreco());
+        System.out.println("Estoque atual: " + getEstoque());
+
+    }
+}
